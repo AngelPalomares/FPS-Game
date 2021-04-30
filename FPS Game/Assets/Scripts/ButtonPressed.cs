4685c6da9ae5de85f84ac3e17cec2dad20ef113a
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2021-04-30 11:42:29 -0500

    Update
    Implementation of button System

--- FPS Game/Assets/Scripts/ButtonPressed.cs
+++ FPS Game/Assets/Scripts/ButtonPressed.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class ButtonPressed : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator anim;
+    public bool ButtonPre;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject buttonPressed, ButtonUp;
+    // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Sword")
         {
+            buttonPressed.SetActive(true);
+            //ButtonUp.SetActive(false);
+            
+            
+            ButtonPre = true;
+            anim.SetBool("IsDown", true);
             Debug.Log("Box is on top of me");
+            
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (other.tag == "Sword")
+        {
+            
+            buttonPressed.SetActive(false);
+            //ButtonUp.SetActive(true);
+            
+            
+            ButtonPre = false;
+            anim.SetBool("IsDown", false);
             Debug.Log("Button is no longer there");
+            
+        }
     }
 }
